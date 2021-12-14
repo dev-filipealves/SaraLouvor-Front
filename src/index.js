bffa50fb29e9552d58494ed843cdb7522fa2c562
dev-filipealves <87864458+dev-filipealves@users.noreply.github.com>
2021-12-14 12:06:52 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './pages/Root';
-import './index.css';
-
-
-ReactDOM.render(<Root />, document.getElementById('root'));
